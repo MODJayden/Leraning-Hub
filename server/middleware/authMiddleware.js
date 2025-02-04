b72e149a60c87e63154594585ca52b7d5d95e235
Michael Owusu Darko <darkolyrical@gmail.com>
2025-02-04 18:54:57 +0000

    added space to authMiddleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = async (req, res, next) => {
   try {
     const authHeader=req.headers[`authorization`]
-    const token=authHeader && authHeader.split("")[1]
+    const token=authHeader && authHeader?.split(" ")[1]
     
 
     if (!token) {
